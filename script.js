3510f36a659e7381840bef712bc4e283401a54a0
MaryUnn <108895652+MaryUnn@users.noreply.github.com>
2024-06-06 01:37:37 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+﻿let filters = document.querySelectorAll('a[data-filter]');
+
+for (let filter of filters) {
+    filter.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        let catId = filter.getAttribute('data-filter');
+        let workCat = document.querySelectorAll('.main-product-menu');
+
+        workCat.forEach(function (c) {
+                if (c.getAttribute('data-family') !== catId) {
+                    c.classList.add('hide');
+                } else {
+                    c.classList.remove('hide');
+                }
+        })
+
+    });
+}
